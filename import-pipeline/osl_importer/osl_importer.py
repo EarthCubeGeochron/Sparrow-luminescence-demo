1d6be9ccafac5762ff98d7e573f75708ac6c7c7b
Daven Quinn <dev@davenquinn.com>
2019-07-19 16:00:57 -0700

    Import basic datasets

--- import-pipeline/osl_importer/osl_importer.py
+++ import-pipeline/osl_importer/osl_importer.py
@@ -1,23 +1,25 @@
-from itertools import chain
 from sparrow.import_helpers import BaseImporter, SparrowImportError
 from datetime import datetime
-from io import StringIO
-from pandas import read_csv, concat
 from math import isnan
 import numpy as N
 from click import echo, secho
 from sqlalchemy.exc import IntegrityError, DataError
 from pandas import read_excel
+import re
 
 def extract_table(fn):
     df = read_excel(fn, header=0, skiprows=[1])
     if not df.columns[0] == "SAMPLE ID":
         raise SparrowImportError("Data file does not match expected format")
+    return df
 
-    n = len(df)
-    echo(f"{n} records")
-
-    raise NotImplementedError()
+title_exp = re.compile("(.+)\s\((\w+)\)")
+def parse_title(fn):
+    name = fn.stem
+    z = title_exp.match(name)
+    project_name = z.group(1)
+    project_prefix = z.group(2)
+    return (project_name, project_prefix)
 
 class OSLImporter(BaseImporter):
     """
@@ -26,5 +28,57 @@ class OSLImporter(BaseImporter):
     authority = "Desert Research Institute"
 
     def import_datafile(self, fn, rec, **kwargs):
+        """
+        Import an individual data file
+        """
+        # Get data
         df = extract_table(fn)
-        raise NotImplementedError()
+        project_name, project_prefix = parse_title(fn)
+
+        n = len(df)
+        echo(f"{n} records")
+
+        # Set up the project
+        project = self.project(project_name)
+        self.db.session.add(project)
+        # We should add the prefix as either
+        # an extra column or unstructured data to the project
+
+        # We only support one PI per project now, but
+        # maybe this should be changed
+        name = df.iloc[0].loc["PI"]
+        researcher = self.researcher(name=name)
+        self.db.session.add(researcher)
+        project.add_researcher(researcher)
+
+        for i, row in df.iterrows():
+            sample = self.create_sample(row)
+            session = self.create_session(row, sample)
+            project.add_session(session)
+            yield sample
+
+
+    def create_sample(self, row):
+        sample_id = str(row["SAMPLE ID"])
+        sample = self.sample(name=sample_id)
+
+        # Import geographic coordinates
+        lat = row.loc["GEOGRAPHIC COORDINATES"]
+        lon = row.iloc[5]
+        sample.location = self.location(lon, lat)
+
+        self.db.session.add(sample)
+
+        return sample
+
+    def create_date(self, row):
+        year = row["YEAR"]
+        if isnan(year):
+            return datetime.min
+        return datetime(int(year), 1, 1)
+
+    def create_session(self, row, sample):
+        return self.db.get_or_create(
+            self.m.session,
+            sample_id=sample.id,
+            date=self.create_date(row))
